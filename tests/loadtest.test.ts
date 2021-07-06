be97be650c0c3b3ab694b8bfd136607398fb8834
Isaac <mcauleyisaac@gmail.com>
2021-07-05 23:54:58 -0600

    Tests for loading instructions

---
+++ tests/loadtest.test.ts
@@ -0,0 +1,14 @@
+import {CPU} from '../src/cpu';
+import {Memory} from '../src/memory';
+
+test('index 0 access', () => {
+    // LDB, 0x04 = 0x0604
+    let program = new Uint16Array([0x0604]);
+    let mem = new Memory(400, program);
+
+    const value = mem.getWord(0);
+
+    let cpu = new CPU(mem);
+    cpu.runCycle();
+    expect(cpu.registers[0].get()).toBe(0x04);
+})
