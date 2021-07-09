e5a34709a0fe4f05bb59a8b37bfb0ea690837bd0
Isaac <mcauleyisaac@gmail.com>
2021-07-08 22:56:59 -0600

    Added ADD test

--- tests/arethmetic.test.ts
+++ tests/arethmetic.test.ts
@@ -12,11 +12,26 @@ test('increments', () => {
 
     let cpu = new CPU(mem);
     cpu.runCycle();
-    // cpu.runCycle();
-    // expect(cpu.registers[0].get()).toBe(0x05);
-    // cpu.runCycle();
-    // expect(cpu.registers[0].get()).toBe(0x06);
-    // cpu.runCycle();
-    // cpu.runCycle();
-    // expect(cpu.registers[0].get()).toBe(0x04);
+    cpu.runCycle();
+    expect(cpu.registers[0].get()).toBe(0x05);
+    cpu.runCycle();
+    expect(cpu.registers[0].get()).toBe(0x06);
+})
+
+test('add B to A', () => {
+    // LD B, 4
+    // LD A, 4
+    // ADD A, B
+    let program = new Uint16Array([0x0604, 0x3E04, 0x8000]);
+    let mem = new Memory(400, program);
+
+    const value = mem.getWord(0);
+
+    let cpu = new CPU(mem);
+    cpu.runCycle();
+    expect(cpu.registers[0].get()).toBe(0x04);
+    cpu.runCycle();
+    expect(cpu.registers[7].get()).toBe(0x04);
+    cpu.runCycle();
+    expect(cpu.registers[7].get()).toBe(0x08);
 })
