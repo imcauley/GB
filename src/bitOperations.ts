0ae2a6062e6ed04891df12998d22c215837ba06f
Isaac <mcauleyisaac@gmail.com>
2021-07-12 18:53:42 -0600

    Filled out double register

--- src/bitOperations.ts
+++ src/bitOperations.ts
@@ -4,4 +4,12 @@ export class BitOperations {
         const low = (word & 0x00FF) << 8;
         return low + high;
     }
+
+    public static getHigh(word: number) {
+        return (word & 0xFF00) >> 8;
+    }
+
+    public static getLow(word: number) {
+        return (word & 0x00FF);
+    }
 }
--- src/doubleRegister.ts
+++ src/doubleRegister.ts
@@ -1,20 +1,28 @@
 import {Register} from './register';
+import {BitOperations} from './bitOperations';
 
 export class DoubleRegister extends Register {
     high: Register;
     low: Register;
 
-    constructor(high: Register, low: Register) {
+    constructor(high?: Register, low?: Register) {
         super();
-        this.high = high;
-        this.low = low;
+        if(high && low) {
+            this.high = high;
+            this.low = low;
+        }
+        else {
+            this.high = new Register();
+            this.low = new Register();
+        }
     }
 
     get() {
-        return this.low.get();
+        return this.low.get() + (this.high.get() << 8);
     }
 
     set(value: number) {
-        return this.low.set(value);
+        this.high.set(BitOperations.getHigh(value))
+        this.low.set(BitOperations.getLow(value));
     }
 }
