3adfb69219ef5babe83966c4b1c72b2f9fcf483f
Isaac <mcauleyisaac@gmail.com>
2021-07-18 18:22:15 -0600

    Added test for bit test

--- tests/control.test.ts
+++ tests/control.test.ts
@@ -1,11 +1,15 @@
 import {CPU} from '../src/cpu';
 import {Memory} from '../src/memory';
 
-test('index 0 access', () => {
+test('test 0 bit', () => {
     // LDB, 0x04 = 0x0604
-    let program = new Uint16Array([0xC324]);
+    let program = new Uint16Array([0x06FF, 0xCB40, 0x06FE, 0xCB40]);
     let mem = new Memory(400, program);
     let cpu = new CPU(mem);
     cpu.runCycle();
-    expect(cpu.programCounter).toBe(0x24);
+    cpu.runCycle();
+    expect(cpu.statusRegister.getFlag('Z')).toBe(false);
+    cpu.runCycle();
+    cpu.runCycle();
+    expect(cpu.statusRegister.getFlag('Z')).toBe(true);
 })
