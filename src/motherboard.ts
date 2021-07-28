8048b771aeb421731acd7fa6495a7bd72cc5c87f
Isaac <mcauleyisaac@gmail.com>
2021-07-27 20:31:56 -0600

    Created motherboard

--- src/motherboard.ts
+++ src/motherboard.ts
@@ -1,8 +1,23 @@
 import {CPU} from './cpu';
-import {Memory} from './memory'
+import {Memory} from './memory';
+import {Clock} from './clock';
 
 class Motherboard {
-    constructor() {
-        
+    cpu: CPU;
+    mem: Memory;
+    clock: Clock;
+
+    constructor(cartridge: Uint16Array) {
+        this.mem = new Memory(4000, cartridge);
+        this.cpu = new CPU(this.mem);
+        this.clock = new Clock();
+
+        const numCycles = 10;
+        let currentCycle = 0;
+
+        this.clock.onHigh(this.cpu.runCycle);
+        this.clock.onHigh(function() {this.currentCycle++});
+
+        this.clock.start(function() {return this.numCycles === this.currentCycle});
     }
 }
